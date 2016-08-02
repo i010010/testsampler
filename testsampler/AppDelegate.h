73869a0ae85b8b1fa4b570a70a87f43d1df710eb
apple <apple@apple-no-macbook-air.local>
2016-08-02 20:31:46 +0900

    Initial Commit

---
+++ testsampler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testsampler
+//
+//  Created by apple on 2016/08/02.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testsampler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testsampler
+//
+//  Created by apple on 2016/08/02.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
